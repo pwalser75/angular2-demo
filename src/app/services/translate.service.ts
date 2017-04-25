140a0a9409604c2cdc83cb23ce6a32f46fddc25c
Peter Walser <pwalser@frostnova.ch>
2017-04-25 12:11:09 +0200

    TranslateService now fires event when a language is loaded or the language is changed

--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -1,15 +1,30 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
+export enum TranslateServiceEventType {
+    LANGUAGE_LOADED,
+    LANGUAGE_CHANGED
+}
+
+export class TranslateServiceEvent {
+    constructor(public type: TranslateServiceEventType, public data: any) {
+    }
+}
 
 @Injectable()
 export class TranslateService {
 
-    currentLanguage:string;
-    supportedLanguages:any;
-    loadedLanguages:any[];
-    translations:any[];
+    private currentLanguage: string;
+    private supportedLanguages: any;
+    private loadedLanguages: any[];
+    private translations: any[];
 
-    constructor(private http:Http) {
+    private eventSource: Subject<TranslateServiceEvent> = new Subject<TranslateServiceEvent>();
+    public events: Observable<TranslateServiceEvent> = this.eventSource.asObservable();
+
+    constructor(private http: Http) {
         this.supportedLanguages = {
             "en": "English",
             "de": "Deutsch"
@@ -22,34 +37,37 @@ export class TranslateService {
         }
     }
 
-    loadLanguageResources(lang:string):void {
-        let resource:string = "app/localization/" + lang + ".json";
+    loadLanguageResources(lang: string): void {
+        let resource: string = "app/localization/" + lang + ".json";
         console.log("Loading: " + resource);
 
         this.http.get(resource)
-            .map((res:any) => res.json())
+            .map((res: any) => res.json())
             .subscribe(
-                data => this.translations[lang] = data,
+                data => {
+                    this.translations[lang] = data;
+                    this.emitEvent(new TranslateServiceEvent(TranslateServiceEventType.LANGUAGE_LOADED, lang));
+                },
                 error => console.log(error)
             );
     }
 
-
-    setLanguage(lang:string):void {
-        if (this.isSupportedLanguage(lang)) {
+    setLanguage(lang: string): void {
+        if (this.isSupportedLanguage(lang) && this.currentLanguage != lang) {
             this.currentLanguage = lang;
+            this.emitEvent(new TranslateServiceEvent(TranslateServiceEventType.LANGUAGE_CHANGED, lang));
         }
     }
 
-    getLanguage():string {
+    getLanguage(): string {
         return this.currentLanguage;
     }
 
-    isCurrentLanguage(lang:string):boolean {
+    isCurrentLanguage(lang: string): boolean {
         return this.currentLanguage === lang;
     }
 
-    getSupportedLanguages():string[] {
+    getSupportedLanguages(): string[] {
         var keys = [];
         for (let lang in this.supportedLanguages) {
             keys.push(lang);
@@ -57,15 +75,15 @@ export class TranslateService {
         return keys;
     }
 
-    isSupportedLanguage(lang:string):boolean {
+    isSupportedLanguage(lang: string): boolean {
         return this.getSupportedLanguages().indexOf(lang) >= 0;
     }
 
-    getLanguageName(lang:string):string {
+    getLanguageName(lang: string): string {
         return this.supportedLanguages[lang];
     }
 
-    translate(key:string):string {
+    translate(key: string): string {
 
         if (!key) return null;
         let languageMap = this.translations[this.currentLanguage];
@@ -74,7 +92,7 @@ export class TranslateService {
             return '...';
         }
         if (key) {
-            let value:any = languageMap;
+            let value: any = languageMap;
             for (let part of key.split(".")) {
                 value = value ? value[part] : null;
             }
@@ -89,7 +107,7 @@ export class TranslateService {
         }
     }
 
-    replacePlaceholders(text:string, replacements:any):string {
+    replacePlaceholders(text: string, replacements: any): string {
         if (!text || !replacements) {
             return text;
         }
@@ -97,5 +115,11 @@ export class TranslateService {
             text = text.replace("${" + placeholder + "}", replacements[placeholder]);
         }
         return text;
+    }
+
+    private emitEvent(event: TranslateServiceEvent) {
+        if (this.eventSource && event) {
+            this.eventSource.next(event);
+        }
     }
 }
--- src/app/widgets/input-component.component.ts
+++ src/app/widgets/input-component.component.ts
@@ -1,22 +1,32 @@
 import {Component, Input, OnChanges, AfterViewInit} from "@angular/core";
-import {TranslateService} from "../services/translate.service";
+import {TranslateService, TranslateServiceEvent, TranslateServiceEventType} from "../services/translate.service";
 
 @Component({
     selector: 'input-component',
     templateUrl: 'templates/input-component.html'
 })
 export class InputComponent implements OnChanges, AfterViewInit {
-    @Input() name:any;
-    @Input() label:any;
-    @Input() errors:any;
+    @Input() name: any;
+    @Input() label: any;
+    @Input() errors: any;
 
-    @Input() control:any;
+    @Input() control: any;
     //@ViewChild(FormControl) control:FormControl;
 
-    errorKey:any;
-    errorMessage:any;
+    errorKey: any;
+    errorValue: any;
+    errorMessage: any;
 
-    constructor(private translateService:TranslateService) {
+    constructor(private translateService: TranslateService) {
+        this.translateService.events.subscribe((event: TranslateServiceEvent) => {
+            if (event.type === TranslateServiceEventType.LANGUAGE_LOADED) {
+                console.log("Language loaded: " + event.data);
+            }
+            if (event.type === TranslateServiceEventType.LANGUAGE_CHANGED) {
+                console.log("Language changed: " + event.data);
+            }
+            this.updateErrorMessage();
+        });
     }
 
     ngAfterViewInit() {
@@ -24,20 +34,25 @@ export class InputComponent implements OnChanges, AfterViewInit {
         // this.footer now points to the instance of `FooterComponent`
     }
 
-    ngOnChanges(changes:any):void {
+    ngOnChanges(changes: any): void {
 
-        var errors:any = changes.errors.currentValue;
-        var key:string = this.getFirstErrorMessageKey(errors);
+        var errors: any = changes.errors.currentValue;
+        var key: string = this.getFirstErrorMessageKey(errors);
         this.errorKey = key;
-        if (key) {
-            var translated = this.translateService.translate("error." + key);
-            this.errorMessage = this.translateService.replacePlaceholders(translated, errors[key]);
+        this.errorValue = key ? errors[key] : null;
+        this.updateErrorMessage();
+    }
+
+    private updateErrorMessage(): void {
+        if (this.errorKey) {
+            var translated = this.translateService.translate("error." + this.errorKey);
+            this.errorMessage = this.translateService.replacePlaceholders(translated, this.errorValue);
         } else {
             this.errorMessage = null;
         }
     }
 
-    getFirstErrorMessageKey(errors:any):string {
+    getFirstErrorMessageKey(errors: any): string {
         if (!errors) return null;
         for (let key in errors) {
             return key;
